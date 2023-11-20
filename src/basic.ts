9a810c69599c7c7b624398566b5a36c8aedc4207
kesleymartins <kesley.martins@vivaldi.net>
2023-11-20 05:53:48 -0300

    add file with basic types

---
+++ src/basic.ts
@@ -0,0 +1,14 @@
+let firstName: string = "Doctor"
+let age: number = 800
+let isFictional: boolean = true
+
+let a: null = null
+let b: undefined = undefined
+
+console.log(firstName)
+console.log(age)
+console.log(isFictional)
+
+console.log(a)
+console.log(b)
+
